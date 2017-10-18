fe3ce0562945b0892367d6d1cda209975e9a35a6
Kashyap <k.patel1098@gmail.com>
2017-10-18 10:45:54 -0500

    Hangman Changes v1.1
    This change allows the user to continue the game and ask for more words.
    
    Todo:
    - Allow difficulty setter.
    - Create scoring system.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,60 +15,68 @@ public class Main {
         int wordLength;
         int emptyCount;
         int lives;
-        Scanner s = new Scanner(System.in);
+        String cont = "yes";
         Player player = new Player();
-
-        ArrayList<String> letters = new ArrayList();
-        ArrayList<String> guessedLetters = new ArrayList();
-        ArrayList<String> wordLetters = new ArrayList();
-
-        String word = "";
-        lives = 10;
-        line = ThreadLocalRandom.current().nextInt(10, max + 1);
-
-        word = Logic.GetWord(line);
-        wordLength = word.length();
-
-        for(int i = 0; i < wordLength; i++){
-
-            letters.add("_");
-            wordLetters.add(String.valueOf(word.charAt(i)));
-
-        }
-
-        System.out.println("Word: " + word);
-        System.out.println("Word Length: " + wordLength);
-        System.out.println(letters);
-        System.out.println(wordLetters);
+        Scanner s = new Scanner(System.in);
 
         do {
-            emptyCount = 0;
+            ArrayList<String> letters = new ArrayList();
+            ArrayList<String> guessedLetters = new ArrayList();
+            ArrayList<String> wordLetters = new ArrayList();
 
-            System.out.println("Enter a letter.");
-            String guessedLetter = String.valueOf(s.nextLine());
+            String word = "";
+            lives = 10;
+            line = ThreadLocalRandom.current().nextInt(10, max + 1);
 
-            guessedLetters.add(guessedLetter);
+            word = Logic.GetWord(line);
+            wordLength = word.length();
 
-            letters = Logic.LetterChecker(player, letters, wordLetters, guessedLetter);
-            System.out.println(letters);
+            for (int i = 0; i < wordLength; i++) {
 
-            for(int i = 0; i < letters.size(); i ++){
-
-                String tempChar = letters.get(i);
-
-                if(tempChar.equalsIgnoreCase("_")){
-
-                    emptyCount++;
-
-                }
+                letters.add("_");
+                wordLetters.add(String.valueOf(word.charAt(i)));
 
             }
 
-            System.out.println("You have " + emptyCount + " spaces left and " + player.getLives() + " lives left.");
-            System.out.println("You have guessed " + guessedLetters);
-        }while(emptyCount != 0);
+            System.out.println("Word: " + word);
+            System.out.println("Word Length: " + wordLength);
+            System.out.println(letters);
+            System.out.println(wordLetters);
 
-        System.out.println("Congrats! Your word was " + word);
+            do {
+                emptyCount = 0;
+
+                System.out.println("Enter a letter.");
+                String guessedLetter = String.valueOf(s.nextLine());
+
+                guessedLetters.add(guessedLetter);
+
+                letters = Logic.LetterChecker(player, letters, wordLetters, guessedLetter);
+                System.out.println(letters);
+
+                for (int i = 0; i < letters.size(); i++) {
+
+                    String tempChar = letters.get(i);
+
+                    if (tempChar.equalsIgnoreCase("_")) {
+
+                        emptyCount++;
+
+                    }
+
+                }
+
+                System.out.println("You have " + emptyCount + " spaces left and " + player.getLives() + " lives left.");
+                System.out.println("You have guessed " + guessedLetters);
+            } while (emptyCount != 0);
+
+            System.out.println("Congrats! Your word was " + word);
+            System.out.println("If you would like to continue enter yes, otherwise enter no.");
+            cont = s.nextLine();
+
+        }while(cont.equalsIgnoreCase("yes"));
+
+        System.out.println("Thanks for playing!");
 
         s.close();
     }
